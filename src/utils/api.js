3150c8963bd0712827fe65d192c52c5a967b1e89
Ganesh Ramar <ganeshramar98@gmail.com>
2026-02-25 21:25:38 +0530

    update client url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || '/api';
+const VITE_API_URL='https://drain-analyzer-be.onrender.com/api';
+const API_URL = VITE_API_URL || '/api';
 
 const api = axios.create({
   baseURL: API_URL,
